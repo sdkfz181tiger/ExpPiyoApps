3f99553cf650a5e4f5667ea776c7972d7946b7a3
Shinichi Ozaki <sdkfz181tiger@gmail.com>
2022-12-24 17:23:45 +0900

    Update

--- Todo/js/main.js
+++ Todo/js/main.js
@@ -1,7 +1,7 @@
 console.log("main.js!!");
 
-// Record
-class Record{
+// Todo
+class Todo{
 	constructor(obj){
 		this._id = obj.id;
 		this._tag = obj.tag;
@@ -28,12 +28,11 @@ const myData = {
 	mode: MODE_LOADING,
 	actives: [false, false, false, false, false],
 	myOffcanvas: null,
-	selectedTag: null,
-	tags: [],
-	records: [],
-	todos: [],
-	todoId: null,
-	todoMsg: null,
+	activeTag: null,
+	data: null,
+	tags: [], todos: [],
+	tagId: null, tagName: null,
+	todoId: null, todoMsg: null
 }
 
 // Vue.js
@@ -49,15 +48,8 @@ const app = Vue.createApp({
 
 		// Axios
 		loadAxios("./js/data.json", (json)=>{
-			// Data
-			const data = json.data;
-			// Tags
-			for(let tag of data.tags) this.tags.push(tag);
-			// Records
-			for(let record of data.records) this.records.push(new Record(record));
-			// Todos
-			this.selectedTag = this.tags[0];// Selected
-			this.todos = this.records.filter(record=>record.tag==this.selectedTag);
+			this.data = json.data;// Data
+			this.changeTag(this.data.tags[0]);
 			setTimeout(()=>{this.changeMode(MODE_HOME);}, 200);
 		}, (err)=>{
 			showToast("Error", "0 min ago", "通信エラーです");
@@ -67,8 +59,10 @@ const app = Vue.createApp({
 		const elemOff = document.getElementById("myOffcanvas");// Offcanvas
 		this.myOffcanvas = new bootstrap.Offcanvas(elemOff);
 		// Modal
-		const elem = document.getElementById("myModal");
-		const modal = new bootstrap.Modal(elem);
+		const elemModalTag = document.getElementById("myModalTag");
+		const modalTag = new bootstrap.Modal(elemModalTag);
+		const elemModalTodo = document.getElementById("myModalTodo");
+		const modalTodo = new bootstrap.Modal(elemModalTodo);
 	},
 	methods:{
 		changeMode(mode){
@@ -78,10 +72,50 @@ const app = Vue.createApp({
 				this.actives[i] = this.mode == i;
 			}
 		},
+		createTag(){
+			console.log("createTag");
+			// Error
+			if(this.tagName == null || this.tagName.length <= 0){
+				showToast("Error", "1 min ago", "タグ名を入力してください");
+				return;
+			}
+			// Tag
+			const tag = {
+				id: "t_" + Date.now(),
+				name: this.tagName
+			}
+			this.data.tags.push(tag);// Create
+			this.activeTag = tag;// Active
+			this.changeTag(this.activeTag);// Reflesh
+		},
+		updateTag(id){
+			console.log("updateTag:", id);
+			for(let i=this.data.tags.length-1; 0<=i; i--){
+				const tag = this.data.tags[i];
+				if(tag.id != id) continue;
+				tag.name = this.tagName;// Update
+				break;
+			}
+			this.changeTag(this.activeTag);// Reflesh
+		},
+		deleteTag(id){
+			console.log("deleteTag:", id);
+			for(let i=this.data.tags.length-1; 0<=i; i--){
+				const tag = this.data.tags[i];
+				if(tag.id != id) continue;
+				this.data.tags.splice(i, 1);// Delete
+				break;
+			}
+			this.changeTag(this.activeTag);// Reflesh
+		},
 		changeTag(tag){
-			console.log("changeTag:", tag);
-			this.selectedTag = tag;// Selected
-			this.todos = this.records.filter(record=>record.tag==this.selectedTag);
+			console.log("changeTag:", tag.id, tag.name);
+			this.tags = [];// Tags
+			for(let obj of this.data.tags) this.tags.push(obj);
+			this.todos = [];// Tags
+			for(let obj of this.data.todos) this.todos.push(new Todo(obj));
+			this.activeTag = tag;// Selected
+			this.todos = this.data.todos.filter(todo=>todo.tag==this.activeTag.id);
 		},
 		createTodo(){
 			console.log("createTodo");
@@ -90,57 +124,76 @@ const app = Vue.createApp({
 				showToast("Error", "1 min ago", "テキストを入力してください");
 				return;
 			}
-			// Record
-			const record = new Record({
-				id: Date.now(),
-				tag: this.selectedTag,
+			// Todo
+			const todo = new Todo({
+				id: "r_" + Date.now(),
+				tag: this.activeTag.id,
 				msg: this.todoMsg,
 				checked: false
 			});
-			this.records.push(record);// Create
-			this.changeTag(this.selectedTag);// Reflesh
-		},
-		toggleTodo(id){
-			console.log("toggleTodo:", id);
-			for(let i=this.records.length-1; 0<=i; i--){
-				const record = this.records[i];
-				if(record.id != id) continue;
-				record.checked = !record.checked;// Toggle
-			}
-			this.changeTag(this.selectedTag);// Reflesh
+			this.data.todos.push(todo);// Create
+			this.changeTag(this.activeTag);// Reflesh
 		},
 		updateTodo(id){
 			console.log("updateTodo:", id);
-			for(let i=this.records.length-1; 0<=i; i--){
-				const record = this.records[i];
-				if(record.id != id) continue;
-				record.msg = this.todoMsg;// Update
+			for(let i=this.data.todos.length-1; 0<=i; i--){
+				const todo = this.data.todos[i];
+				if(todo.id != id) continue;
+				todo.msg = this.todoMsg;// Update
+				break;
 			}
-			this.changeTag(this.selectedTag);// Reflesh
+			this.changeTag(this.activeTag);// Reflesh
 		},
 		deleteTodo(id){
 			console.log("deleteTodo:", id);
-			for(let i=this.records.length-1; 0<=i; i--){
-				const record = this.records[i];
-				if(record.id != id) continue;
-				this.records.splice(i, 1);// Delete
+			for(let i=this.data.todos.length-1; 0<=i; i--){
+				const todo = this.data.todos[i];
+				if(todo.id != id) continue;
+				this.data.todos.splice(i, 1);// Delete
+				break;
 			}
-			this.changeTag(this.selectedTag);// Reflesh
+			this.changeTag(this.activeTag);// Reflesh
 		},
-		showCreateModal(){
-			console.log("showCreateModal");
-			this.todoId = null;
-			this.todoMsg = null;
-			const elem = document.getElementById("myModal");
-			elem.querySelector("#modalLabel").innerText = "Create";
+		toggleTodo(id){
+			console.log("toggleTodo:", id);
+			for(let i=this.data.todos.length-1; 0<=i; i--){
+				const todo = this.data.todos[i];
+				if(todo.id != id) continue;
+				todo.checked = !todo.checked;// Toggle
+				break;
+			}
+			this.changeTag(this.activeTag);// Reflesh
+		},
+		showModalCreateTag(){
+			console.log("showModalCreateTag");
+			this.tagId = null;
+			this.tagName = null;
+			const elem = document.getElementById("myModalTag");
+			elem.querySelector("#modalLabel").innerText = "CreateTag";
 			bootstrap.Modal.getInstance(elem).show();
 		},
-		showEditModal(todo){
-			console.log("showEditModal:", todo.id);
+		showModalEditTag(tag){
+			console.log("showModalEditTag:", tag.id);
+			this.tagId = tag.id;
+			this.tagName = tag.name;
+			const elem = document.getElementById("myModalTag");
+			elem.querySelector("#modalLabel").innerText = "EditTag";
+			bootstrap.Modal.getInstance(elem).show();
+		},
+		showModalCreateTodo(){
+			console.log("showModalCreateTodo");
+			this.todoId = null;
+			this.todoMsg = null;
+			const elem = document.getElementById("myModalTodo");
+			elem.querySelector("#modalLabel").innerText = "CreateTodo";
+			bootstrap.Modal.getInstance(elem).show();
+		},
+		showModalEditTodo(todo){
+			console.log("showModalEditTodo:", todo.id);
 			this.todoId = todo.id;
 			this.todoMsg = todo.msg;
-			const elem = document.getElementById("myModal");
-			elem.querySelector("#modalLabel").innerText = "Edit";
+			const elem = document.getElementById("myModalTodo");
+			elem.querySelector("#modalLabel").innerText = "EditTodo";
 			bootstrap.Modal.getInstance(elem).show();
 		}
 	}
