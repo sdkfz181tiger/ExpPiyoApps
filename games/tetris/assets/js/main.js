ac3a55212aab62ff7336f4a18abfe9e5a651df59
Kajiru <sdkfz181tiger@gmail.com>
2023-01-07 17:34:19 +0900

    Update

--- games/tetris/assets/js/main.js
+++ games/tetris/assets/js/main.js
@@ -2,8 +2,9 @@ console.log("main.js!!");
 
 const TITLE = "テトリス";
 
-let cX, cY, rSize, tMng;
+let rSize, cX, cY, tMng;
 let font, btnHome;
+let btnLeft, btnRight, btnDown, btnRoll;
 
 function preload(){
 	font = loadFont("./assets/fonts/nicokaku_v2.ttf");
@@ -17,15 +18,33 @@ function setup(){
 	btnHome = new MyButton("caret-l-w.png", ()=>{
 		window.location.replace("../../");
 	});
+	btnLeft = new MyButton("caret-l-w.png", ()=>{
+		actionLeft();
+	});
+	btnRight = new MyButton("caret-r-w.png", ()=>{
+		actionRight();
+	});
+	btnDown = new MyButton("caret-d-w.png", ()=>{
+		actionDown();
+	});
+	btnRoll = new MyButton("arrow-roll-r-w.png", ()=>{
+		actionRoll();
+	});
 	textFont(font);
 	frameRate(32);
 	noSmooth();
 
 	// Tetris
+	rSize = Math.floor(height * 0.032);
 	cX = width / 2;
-	cY = height / 2;
-	rSize = Math.floor(height * 0.025);
+	cY = height / 2 - rSize * 1;
 	tMng = new TetrisManager();
+
+	// Reposition
+	btnLeft.setPos(cX-rSize*5.5, cY+rSize*(T_ROWS+4)*0.5, rSize * 1.5);
+	btnRight.setPos(cX-rSize*2.5, cY+rSize*(T_ROWS+4)*0.5, rSize * 1.5);
+	btnDown.setPos(cX-rSize*4, cY+rSize*(T_ROWS+7)*0.5, rSize * 1.5);
+	btnRoll.setPos(cX+rSize*4.5, cY+rSize*(T_ROWS+5)*0.5, rSize * 3);
 }
 
 function draw(){
@@ -34,6 +53,10 @@ function draw(){
 	textSize(28); textAlign(RIGHT, BASELINE);
 	text(TITLE, width - 12, 32);
 	btnHome.drawBtn();
+	btnLeft.drawBtn();
+	btnRight.drawBtn();
+	btnDown.drawBtn();
+	btnRoll.drawBtn();
 
 	// Frame
 	fill("#DDDDDD");
@@ -58,44 +81,57 @@ function draw(){
 	fill("#333333");
 	textSize(rSize*1.5); textAlign(CENTER);
 	text("TETRIS", cX, cY-rSize*T_ROWS/2-rSize*1.0);
-	textSize(rSize*0.8); textAlign(CENTER);
-	text("LEFT key: move left.\nRIGHT key: move right.\nUP key: rotate.\n",
-		cX, cY+rSize*T_ROWS/2+rSize*1.5);
 }
 
 function mousePressed(){
 	btnHome.checkBtn();
+	btnLeft.checkBtn();
+	btnRight.checkBtn();
+	btnDown.checkBtn();
+	btnRoll.checkBtn();
 }
 
 function touchStarted(){
 	btnHome.checkBtn();
+	btnLeft.checkBtn();
+	btnRight.checkBtn();
+	btnDown.checkBtn();
+	btnRoll.checkBtn();
 }
 
 function keyPressed(){
-	if(keyCode == LEFT_ARROW){
-		if(tMng.checkWallL()) return;
-		tMng.stepLeft();
-		if(tMng.checkCollision()) tMng.stepRight();
+	if(keyCode == LEFT_ARROW) actionLeft();
+	if(keyCode == RIGHT_ARROW) actionRight();
+	if(keyCode == UP_ARROW) actionRoll();
+	if(keyCode == DOWN_ARROW) actionDown();
+}
+
+function actionLeft(){
+	if(tMng.checkWallL()) return;
+	tMng.stepLeft();
+	if(tMng.checkCollision()) tMng.stepRight();
+}
+
+function actionRight(){
+	if(tMng.checkWallR()) return;
+	tMng.stepRight();
+	if(tMng.checkCollision()) tMng.stepLeft();
+}
+
+function actionRoll(){
+	tMng.rotateL();
+	if(tMng.checkCollision()){
+		tMng.rotateR();
+	}else{
+		tMng.checkRotation();
 	}
-	if(keyCode == RIGHT_ARROW){
-		if(tMng.checkWallR()) return;
-		tMng.stepRight();
-		if(tMng.checkCollision()) tMng.stepLeft();
-	}
-	if(keyCode == UP_ARROW){
-		tMng.rotateL();
-		if(tMng.checkCollision()){
-			tMng.rotateR();
-		}else{
-			tMng.checkRotation();
-		}
-	}
-	if(keyCode == DOWN_ARROW){
-		tMng.stepDown();
-		if(tMng.checkCollision()){
-			tMng.stepUp();
-			tMng.fixMino();
-			tMng.createMino();
-		}
+}
+
+function actionDown(){
+	tMng.stepDown();
+	if(tMng.checkCollision()){
+		tMng.stepUp();
+		tMng.fixMino();
+		tMng.createMino();
 	}
 }
