709a82b4981bc3e6b83431d21561e9935e9b0fe3
Kajiru <sdkfz181tiger@gmail.com>
2023-01-16 19:37:21 +0900

    Update

--- contents/games/minesweeper/assets/js/main.js
+++ contents/games/minesweeper/assets/js/main.js
@@ -8,7 +8,11 @@ const FILES_IMG = [
 	"caret-l-b.png", "caret-r-b.png"
 ];
 
+const MS_GRIDS = 8;
+const MS_MINES = 8;
+
 let font, cX, cY;
+let gSize, mMng, tiles;
 
 function preload(){
 	font = loadFont("./assets/fonts/nicokaku_v2.ttf");
@@ -26,6 +30,25 @@ function setup(){
 
 	cX = Math.floor(width * 0.5);
 	cY = Math.floor(height * 0.5);
+
+	// MineSweeper
+	gSize = Math.floor((width<height) ? width*0.1:height*0.1);
+	mMng = new MineSweeperManager(MS_GRIDS, MS_GRIDS, MS_MINES);
+	//mMng.consoleAll();
+
+	// Tiles
+	tiles = [];
+	let mine = mMng.getMine();
+	let sensor = mMng.getSensor();
+	let total = MS_GRIDS**2;
+	for(let t=0; t<total; t++){
+		let r = Math.floor(t / MS_GRIDS);
+		let c = Math.floor(t % MS_GRIDS);
+		let m = mine[r][c];
+		let s = sensor[r][c];
+		let tile = new Tile(r, c, gSize, m, s);
+		tiles.push(tile);
+	}
 }
 
 function draw(){
@@ -33,8 +56,47 @@ function draw(){
 	noStroke(); fill("#333333");
 	textSize(FONT_SIZE); textAlign(RIGHT, BASELINE);
 
+	// Title
+	for(let tile of tiles) tile.draw();
+
 	// Mine
 	fill("#333333");
 	textSize(FONT_SIZE); textAlign(CENTER, TOP);
 	text("MINE:xx", cX, FONT_SIZE * 0.5);
+}
+
+function mousePressed(){
+	checkTiles();
+}
+
+function touchStarted(){
+	checkTiles();
+}
+
+function checkTiles(){
+	for(let tile of tiles){
+		if(tile.mousePressed(mouseX, mouseY)){
+			mineSweep(tile.getR(), tile.getC());
+			break;
+		}
+	}
+}
+
+function mineSweep(r, c){
+	// Minesweeper
+	if(!mMng.sweep(r, c)){
+		console.log("CONTINUE!!");
+		for(let t=0; t<tiles.length; t++){
+			let r = Math.floor(t / MS_GRIDS);
+			let c = Math.floor(t % MS_GRIDS);
+			let search = mMng.getSearch();
+			if(search[r][c] == 1){
+				tiles[t].open();
+			}
+		}
+	}else{
+		console.log("GAME OVER!!");
+		let t = r * MS_GRIDS + c;
+		tiles[t].open();
+	}
 }
