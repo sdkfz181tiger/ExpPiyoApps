c781b7d74ee4697765ecfd5090cb6f3dad235da6
Kajiru <sdkfz181tiger@gmail.com>
2024-10-21 09:59:05 +0900

    Update

--- contents/p5js/template/assets/js/main.js
+++ contents/p5js/template/assets/js/main.js
@@ -1,14 +1,15 @@
 console.log("main.js!!");
 
 const FONT_SIZE = 28;
-const GAME_W = 320;
-const GAME_H = 480;
+const A_RACIO   = 3/4;
+const AD_HEIGHT = 120;
 
 const FILES_IMG = [
 	"caret-l-b.png", "caret-r-b.png"
 ];
 
-let font, cX, cY, sX, sY;
+let font, cW, cH, cX, cY;
+let gSize, rows, cols;
 
 function preload(){
 	font = loadFont("../../assets/fonts/nicokaku_v2.ttf");
@@ -16,25 +17,27 @@ function preload(){
 }
 
 function setup(){
-	const cW = window.innerWidth;
-	const cH = window.innerHeight;
+	const main = document.querySelector("main");
+	cH = main.clientHeight - AD_HEIGHT;
+	cW = min(main.clientWidth, cH*A_RACIO);
+	cX = cW / 2;
+	cY = cH / 2;
+	gSize = floor(cW / 20);
+	rows = floor(cH / gSize);
+	cols = floor(cW / gSize);
+
 	const canvas = createCanvas(cW, cH);
 	canvas.parent("game");
 	textFont(font);
 	frameRate(8);
 	noSmooth();
-
-	cX = cW / 2;
-	cY = cH / 2;
-	sX = cX - GAME_W / 2;
-	sY = cY - GAME_H / 2;
 }
 
 function draw(){
-	background("#333366");
-	noStroke(); fill("#333333");
+	background("#333333");
+	noStroke(); fill("#cccccc");
 	textSize(FONT_SIZE); textAlign(RIGHT, BASELINE);
-	rect(sX, sY, GAME_W, GAME_H);
+	drawGrids();// Grid
 }
 
 function mousePressed(){
@@ -46,9 +49,14 @@ function touchStarted(){
 	console.log("touchStarted!!");
 }
 
-function drawCircle(x, y, r){
-	noFill();
-	stroke("darkred");
-	strokeWeight(10);
-	circle(x+r/2, y+r/2, r/2);
+function drawGrids(){
+	stroke("silver"); strokeWeight(1);
+	for(let r=0; r<rows+1; r++){
+		const y = r * gSize;
+		line(0, y, cW, y);
+		for(let c=0; c<cols+1; c++){
+			const x = c * gSize;
+			line(x, 0, x, cH);
+		}
+	}
 }
