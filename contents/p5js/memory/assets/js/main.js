29f86b49cb964d902dcd9f686c2a30d1bec21bed
Kajiru <sdkfz181tiger@gmail.com>
2024-10-27 21:29:18 +0900

    Update

---
+++ contents/p5js/memory/assets/js/main.js
@@ -0,0 +1,108 @@
+console.log("main.js!!");
+
+const FONT_SIZE = 28;
+const A_RACIO   = 3/4;
+const AD_HEIGHT = 120;
+const KEY_HIGH  = "memory";
+
+const FILES_IMG = [
+	"egg_01.png"
+];
+
+const TILE_COLORS = [
+	"#ff595e", "#ffca3a", "#8ac926", "#1982c4", "#6a4c93"
+];
+
+let font, cW, cH, cX, cY;
+let eggMan, cntTap;
+
+function preload(){
+	font = loadFont("../../assets/fonts/nicokaku_v2.ttf");
+	for(let file of FILES_IMG) ImgLoader.loadImg(file);
+}
+
+function setup(){
+	const main = document.querySelector("main");
+	cH = main.clientHeight - AD_HEIGHT;
+	cW = min(main.clientWidth, cH*A_RACIO);
+	cX = cW / 2;
+	cY = cH / 2;
+	gSize = floor(cW / 20);
+	gRows = floor(cH / gSize);
+	gCols = floor(cW / gSize);
+
+	// Canvas
+	const canvas = createCanvas(cW, cH);
+	canvas.parent("game");
+	textFont(font);
+	frameRate(48);
+	noSmooth();
+
+	eggMan = new EggMan("egg_01.png", cX, cY+gSize*2, gSize*16);// EggMan
+	cntTap = loadCounter();// Counter
+}
+
+function draw(){
+	background("#333333");
+	noStroke(); fill("#cccccc");
+	textSize(FONT_SIZE); textAlign(CENTER, CENTER);
+	drawGrids();// Grids
+
+	eggMan.update();// EggMan
+	drawMsgCounter(cX, cY-gSize*10);// Counter
+
+	TWEEN.update();// Tween
+}
+
+function mousePressed(){
+	if(FLG_MOBILE) return;
+	touchStarted();
+}
+
+function touchStarted(){
+	if(mouseY < 0) return;
+	// Shake
+	if(eggMan.contains(mouseX, mouseY)){
+		eggMan.shake(gSize/2);
+		cntTap++;
+		saveCounter();
+		randomDialog();
+	}
+}
+
+function drawGrids(){
+	stroke("#111111"); strokeWeight(1);
+	for(let r=0; r<gRows+1; r++){
+		const y = r * gSize;
+		line(0, y, cW, y);
+		for(let c=0; c<gCols+1; c++){
+			const x = c * gSize;
+			line(x, 0, x, cH);
+		}
+	}
+}
+
+function drawMsgCounter(x, y){
+	fill("#ffffff");
+	textSize(gSize * 2.0); 
+	textAlign(CENTER, CENTER);
+	text(cntTap, x, y);
+}
+
+function loadCounter(){
+	const num = localStorage.getItem(KEY_HIGH);
+	if(num == null) return 0;
+	return num;
+}
+
+function saveCounter(){
+	localStorage.setItem(KEY_HIGH, cntTap);
+}
+
+function randomDialog(){
+	if(1 < random(1000)) return; 
+	const elem = document.getElementById("myModal");
+	if(elem.classList.contains("show")) return;
+	const modal = new bootstrap.Modal(elem);
+	bootstrap.Modal.getInstance(elem).show();
+}
---
+++ contents/p5js/memory/assets/js/sprite.js
@@ -0,0 +1,108 @@
+console.log("utility.js!!");
+
+//==========
+// ImgLoader
+
+class ImgLoader{
+
+	static dir = "./assets/images/";
+	static imgs = {};
+	
+	static loadImg(file){
+		this.imgs[file] = loadImage(this.dir + file);
+	}
+
+	static getImg(file){
+		return this.imgs[file];
+	}
+}
+
+//==========
+// Sprite
+
+class Sprite{
+
+	constructor(file, x, y, size, alpha=255, rotation=0){
+		this._img      = ImgLoader.getImg(file);
+		this._pos      = {x: x, y: y};
+		this._scale    = size / this._img.width;
+		this._alpha    = alpha;
+		this._rotation = rotation;
+		this._visible  = true;
+		this._w        = this._img.width * this._scale;
+		this._h        = this._img.height * this._scale;
+		this._hw       = this._w / 2;
+		this._hh       = this._h / 2;
+	}
+
+	get img(){return this._img;}
+	set img(n){this._img = n;}
+	get x(){return this._pos.x;}
+	set x(n){this._pos.x = n;}
+	get y(){return this._pos.y;}
+	set y(n){this._pos.y = n;}
+
+	get scale(){return this._scale;}
+	set scale(n){
+		this._scale = n;
+		this.w = this._img.width * n;
+		this.h = this._img.height * n;
+	}
+	get alpha(){return this._alpha;}
+	set alpha(n){this._alpha = n;}
+	get rotation(){return this._rotation;}
+	set rotation(n){this._rotation = n;}
+	get visible(){return this._visible;}
+	set visible(n){this._visible = n;}
+
+	get w(){return this._w;}
+	set w(n){
+		this._w = n;
+		this._hw = n / 2;
+	}
+	get h(){return this._h;}
+	set h(n){
+		this._h = n;
+		this._hh = n / 2;
+	}
+
+	get hw(){return this._hw;}
+	get hh(){return this._hh;}
+
+	get l(){return this._pos.x-this._hw;}
+	get r(){return this._pos.x+this._hw;}
+	get t(){return this._pos.y-this._hh;}
+	get b(){return this._pos.y+this._hh;}
+
+	contains(x, y){
+		if(!this._visible) return false;
+		if(x < this.l) return false;
+		if(this.r < x) return false;
+		if(y < this.t) return false;
+		if(this.b < y) return false;
+		return true;
+	}
+
+	intersects(spr){
+		if(!this._visible) return false;
+		if(spr.r < this.l) return false;
+		if(this.r < spr.l) return false;
+		if(spr.b < this.t) return false;
+		if(this.b < spr.t) return false;
+		return true;
+	}
+
+	update(){
+		if(!this._visible) return;
+		tint(255, this._alpha);
+		if(this._rotation == 0){
+			image(this._img, this.l, this.t, this.w, this.h);
+		}else{
+			push();
+			translate(this._pos.x, this._pos.y);
+			rotate(this._rotation * PI/180);
+			image(this._img, -this.hw, -this.hh, this.w, this.h);
+			pop();
+		}
+	}
+}
