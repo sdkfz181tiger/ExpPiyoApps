bbec6efdc89988c7d484f593a8d18f64372b8950
Kajiru <sdkfz181tiger@gmail.com>
2024-11-03 16:37:41 +0900

    Update

---
+++ contents/p5js/tc25/assets/js/main.js
@@ -0,0 +1,99 @@
+console.log("main.js!!");
+
+const FONT_SIZE = 28;
+const A_RACIO   = 3/4;
+const AD_HEIGHT = 120;
+const KEY_HIGH  = "tc25";
+
+const FILES_IMG = [
+	"a_panda.png", "a_bear.png"
+];
+
+let font, cW, cH, cX, cY;
+let cntScore, cntHigh;
+let btnRetryDialog;
+
+function preload(){
+	font = loadFont("../../assets/fonts/nicokaku_v2.ttf");
+	for(let file of FILES_IMG) ImgLoader.loadImg(file);
+}
+
+function setup(){
+	const main = document.querySelector("main");
+	cH = main.clientHeight - AD_HEIGHT;
+	cW = min(main.clientWidth, cH*A_RACIO);
+	cX = cW / 2;
+	cY = cH / 2;
+	gSize = floor(cW / 20);
+	gRows = floor(cH / gSize);
+	gCols = floor(cW / gSize);
+
+	// Canvas
+	const canvas = createCanvas(cW, cH);
+	canvas.parent("game");
+	textFont(font);
+	frameRate(48);
+	noSmooth();
+
+	// Score
+	cntScore = 0;
+	cntHigh = loadHigh();
+
+	// RetryDialog
+	btnRetryDialog = new Button(cX, cY+gSize*11, gSize*6, gSize*2.2, 
+		"RETRY", "#ff595e", true, ()=>{showRetryDialog();});
+}
+
+function draw(){
+	background("#333333");
+	noStroke(); fill("#cccccc");
+	textSize(FONT_SIZE); textAlign(CENTER, CENTER);
+	drawGrids();// Grids
+
+	drawMsg("スコア:"+cntScore, gSize, gSize*1.5, gSize*1.4, LEFT);// Score
+	drawMsg("ハイ:"+cntHigh, gSize, gSize*3.0, gSize*1.0, LEFT, "#ff595e");// High
+
+	btnRetryDialog.update();// RetyDialog
+
+	TWEEN.update();// Tween
+}
+
+function mousePressed(){
+	if(FLG_MOBILE) return;
+	touchStarted();
+}
+
+function touchStarted(){
+	if(mouseY < 0) return;
+	btnRetryDialog.touch(mouseX, mouseY);// RetryDialog
+}
+
+function drawGrids(){
+	stroke("#111111"); strokeWeight(1);
+	for(let r=0; r<gRows+1; r++){
+		const y = r * gSize;
+		line(0, y, cW, y);
+		for(let c=0; c<gCols+1; c++){
+			const x = c * gSize;
+			line(x, 0, x, cH);
+		}
+	}
+}
+
+function drawMsg(msg, x, y, size, align, color="#ffffff"){
+	fill(color);
+	textSize(size);
+	textAlign(align, CENTER);
+	text(msg, x, y);
+}
+
+function loadHigh(){
+	const num = localStorage.getItem(KEY_HIGH);
+	if(num == null) return 0;
+	return num;
+}
+
+function saveHigh(){
+	cntHigh = max(cntHigh, cntScore);
+	localStorage.setItem(KEY_HIGH, cntHigh);
+}
---
+++ contents/p5js/tc25/assets/js/utility.js
@@ -0,0 +1,153 @@
+console.log("utility.js!!");
+
+//==========
+// ServiceWorker
+navigator.serviceWorker.register("./pwa_sw.js");
+
+//==========
+// Mobile
+const FLG_MOBILE = "ontouchstart" in window || 0<navigator.maxTouchPoints;
+
+//==========
+// Axios
+function loadAxios(url, onSuccess, onError){
+	const option = {responseType: "blob"};
+	axios.get(url, option).then(res=>{
+		res.data.text().then(str=>{
+			onSuccess(JSON.parse(str));
+		});
+	}).catch(err=>{
+		onError(err);
+	});
+}
+
+//==========
+// imobile
+function loadImobile(path){
+	// Axios
+	loadAxios(path, json=>{
+		const type = (navigator.userAgent.match(/iPhone|Android.+Mobile/))?"sp":"pc";
+		const places = [
+			{"ad": "banner", "id": "ad_banner"},
+			{"ad": "rectangle", "id": "ad_rectangle"}];
+		for(place of places){
+			const ad = place["ad"];
+			const id = place["id"];
+			const params = json[ad][type];
+			(window.adsbyimobile=window.adsbyimobile||[]).push({
+				pid:params["pid"], mid:params["mid"], asid:params["asid"], 
+				type:"banner", display:"inline", elementid:id});
+		}
+		const elem = document.getElementsByTagName("body")[0];
+		const imobile = document.createElement("script");
+		imobile.src = "//imp-adedge.i-mobile.co.jp/script/v1/spot.js?20220104";
+		imobile.setAttribute("async", "true");
+		elem.appendChild(imobile);
+	}, (err)=>{
+		showToast("Error", "0 min ago", "通信エラーです");
+	});
+}
+
+//==========
+// Modal
+const btnModal = document.getElementById("btn_modal");
+btnModal.addEventListener("click", ()=>{
+	showRetryDialog();
+});
+
+const btnRetry = document.getElementById("btn_retry");
+btnRetry.addEventListener("click", ()=>{
+	location.reload();// Reload
+});
+
+function showRetryDialog(){
+	const elem = document.getElementById("myModal");
+	if(elem.classList.contains("show")) return;
+	const modal = new bootstrap.Modal(elem);
+	bootstrap.Modal.getInstance(elem).show();
+}
+
+//==========
+// Button
+class Button{
+
+	constructor(x, y, w, h, str="HELLO", 
+		color="#444444", visible=true, onPressed=null){
+		this._pos       = {x: x, y: y};
+		this._w         = w;
+		this._h         = h;
+		this._str       = str;
+		this._color     = color;
+		this._visible   = visible;
+		this._onPressed = onPressed;
+	}
+
+	isInside(tX, tY){
+		if(tX < this._pos.x-this._w/2) return false;
+		if(tY < this._pos.y-this._h/2) return false;
+		if(this._pos.x+this._w/2 < tX) return false;
+		if(this._pos.y+this._h/2 < tY) return false;
+		return true;
+	}
+
+	touch(tX, tY){
+		if(!this.isInside(tX, tY)) return false;
+		if(!this._visible) return false;
+		if(this._onPressed) this._onPressed();
+		return true;
+	}
+
+	show(){
+		if(this._visible) return;
+		this._visible = true;
+	}
+
+	update(){
+		if(!this._visible) return false;
+		fill(this._color);
+		noStroke();
+		rectMode(CENTER, CENTER);
+		rect(this._pos.x, this._pos.y, this._w, this._h, this._h*0.1);
+		fill("white"); noStroke();
+		textSize(this._h*0.5); textAlign(CENTER, CENTER);
+		text(this._str, this._pos.x, this._pos.y-this._h*0.04);
+	}
+}
+
+//==========
+// Countdown
+class Countdown{
+
+	constructor(x, y, size, onFinished=null){
+		this._cntMax = 10000;
+		this._cntMin = 1000;
+		this._cntNum = this._cntMax;
+		this._x = x;
+		this._y = y;
+		this._size = size;
+		this._onFinished = onFinished;
+	}
+
+	tick(num){
+		if(this._cntNum <= 0) return;
+		this._cntNum -= num;
+		if(this._cntMin < this._cntNum) return;
+		this._cntNum = 0;
+		if(this._onFinished) this._onFinished();
+	}
+
+	update(){
+		fill("white");
+		const sec = floor(this._cntNum/1000);
+		const mil = this._cntNum % 1000;
+		textSize(this._size*1.0); 
+		textAlign(CENTER, CENTER);
+		text(sec, this._x, this._y);
+		textSize(this._size*0.5);
+		textAlign(CENTER, TOP);
+		text("TIME", this._x, this._y - this._size*0.8);
+		textSize(this._size*0.5);
+		textAlign(CENTER, TOP);
+		text(mil, this._x, this._y + this._size*0.52);
+	}
+}
