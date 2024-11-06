924f4082c70db16acdfac7f00eef55568737096e
Kajiru <sdkfz181tiger@gmail.com>
2024-11-06 20:46:54 +0900

    Update

---
+++ contents/p5js/highandlow/assets/js/main.js
@@ -0,0 +1,147 @@
+console.log("main.js!!");
+
+const FONT_SIZE = 28;
+const A_RACIO   = 3/4;
+const AD_HEIGHT = 120;
+const KEY_HIGH  = "highandlow";
+
+const FILES_IMG = [
+	"card_back_01.png",  "card_back_02.png",  "card_back_04.png",    "card_back_03.png",
+	"card_spade_01.png", "card_heart_01.png", "card_diamond_01.png", "card_club_01.png",
+	"card_spade_02.png", "card_heart_02.png", "card_diamond_02.png", "card_club_02.png",
+	"card_spade_03.png", "card_heart_03.png", "card_diamond_03.png", "card_club_03.png",
+	"card_spade_04.png", "card_heart_04.png", "card_diamond_04.png", "card_club_04.png",
+	"card_spade_05.png", "card_heart_05.png", "card_diamond_05.png", "card_club_05.png",
+	"card_spade_06.png", "card_heart_06.png", "card_diamond_06.png", "card_club_06.png",
+	"card_spade_07.png", "card_heart_07.png", "card_diamond_07.png", "card_club_07.png",
+	"card_spade_08.png", "card_heart_08.png", "card_diamond_08.png", "card_club_08.png",
+	"card_spade_09.png", "card_heart_09.png", "card_diamond_09.png", "card_club_09.png",
+	"card_spade_10.png", "card_heart_10.png", "card_diamond_10.png", "card_club_10.png",
+	"card_spade_11.png", "card_heart_11.png", "card_diamond_11.png", "card_club_11.png",
+	"card_spade_12.png", "card_heart_12.png", "card_diamond_12.png", "card_club_12.png",
+	"card_spade_13.png", "card_heart_13.png", "card_diamond_13.png", "card_club_13.png"
+];
+
+const SUITS = ["spade", "heart", "diamond", "club"];
+
+let font, cW, cH, cX, cY;
+let cntTap, cardSelected;
+let btnRetryDialog;
+
+const trump = [];
+const cards = [];
+
+function preload(){
+	font = loadFont("../../assets/fonts/nicokaku_v2.ttf");
+	for(let file of FILES_IMG) ImgLoader.loadImg(file);
+}
+
+function setup(){
+	const main = document.querySelector("main");
+	cH = main.clientHeight - AD_HEIGHT;
+	cW = min(main.clientWidth, cH*A_RACIO);
+	cX = cW / 2;
+	cY = cH / 2;
+	gSize = floor(cW / 20);
+	gRows = floor(cH / gSize);
+	gCols = floor(cW / gSize);
+
+	// Canvas
+	const canvas = createCanvas(cW, cH);
+	canvas.parent("game");
+	textFont(font);
+	frameRate(32);
+	noSmooth();
+
+	cntTap = loadCounter();// Counter
+	cardSelected = [];// Selected
+
+	// RetryDialog
+	btnRetryDialog = new Button(cX, cY+gSize*12, gSize*6, gSize*2.2, 
+		"RETRY", "#ff595e", true, ()=>{showRetryDialog();});
+
+	// Trump
+	for(let i=0; i<13; i++){
+		for(let s=SUITS.length-1; 0<=s; s--){
+			const rdm = floor(random(s));
+			[SUITS[s], SUITS[rdm]] = [SUITS[rdm], SUITS[s]];
+		}
+		for(const suit of SUITS){
+			const file = "card_" + suit + "_" + String(i+1).padStart(2, "0") + ".png";
+			const card = new Card("card_back_03.png", file, cX, cY, gSize*4);
+			trump.push(card);
+		}
+	}
+
+	// Shuffle with 2 pairs
+	for(let i=24; 0<=i; i--){
+		const rdm = floor(random(i));
+		[trump[i*2], trump[rdm*2]] = [trump[rdm*2], trump[i*2]];
+		[trump[i*2+1], trump[rdm*2+1]] = [trump[rdm*2+1], trump[i*2+1]];
+	}
+}
+
+function draw(){
+	background("#333333");
+	noStroke(); fill("#cccccc");
+	textSize(FONT_SIZE); textAlign(CENTER, CENTER);
+	drawGrids();// Grids
+
+	drawMsgCounter(cX, cY-gSize*11);// Counter
+	btnRetryDialog.update();// RetyDialog
+	for(const card of cards){
+		card.update();// Cards
+	}
+
+	TWEEN.update();// Tween
+}
+
+function mousePressed(){
+	if(FLG_MOBILE) return;
+	touchStarted();
+}
+
+function touchStarted(){
+	if(mouseY < 0) return;
+
+	cntTap++;// Counter
+
+	btnRetryDialog.touch(mouseX, mouseY);// RetryDialog
+
+	// Pickup card
+	cards.push(trump[trump.length-1]);
+	trump.splice(trump.length-1, 1);
+
+	// Slide
+	const card = cards[cards.length-1];
+	card.open(gSize);
+}
+
+function drawGrids(){
+	stroke("#111111"); strokeWeight(1);
+	for(let r=0; r<gRows+1; r++){
+		const y = r * gSize;
+		line(0, y, cW, y);
+		for(let c=0; c<gCols+1; c++){
+			const x = c * gSize;
+			line(x, 0, x, cH);
+		}
+	}
+}
+
+function drawMsgCounter(x, y){
+	fill("#ffffff");
+	textSize(gSize * 2.0); 
+	textAlign(CENTER, CENTER);
+	text(cntTap, x, y);
+}
+
+function loadCounter(){
+	const num = localStorage.getItem(KEY_HIGH);
+	if(num == null) return 0;
+	return num;
+}
+
+function saveCounter(){
+	localStorage.setItem(KEY_HIGH, cntTap);
+}
---
+++ contents/p5js/highandlow/assets/js/utility.js
@@ -0,0 +1,246 @@
+console.log("utility.js!!");
+
+//==========
+// ServiceWorker
+navigator.serviceWorker.register("./pwa_sw.js");
+
+//==========
+// Mobile
+const FLG_MOBILE = "ontouchstart" in window || 0<navigator.maxTouchPoints;
+
+//==========
+// Axios
+function loadAxios(url, onSuccess, onError){
+	const option = {responseType: "blob"};
+	axios.get(url, option).then(res=>{
+		res.data.text().then(str=>{
+			onSuccess(JSON.parse(str));
+		});
+	}).catch(err=>{
+		onError(err);
+	});
+}
+
+//==========
+// imobile
+function loadImobile(path){
+	// Axios
+	loadAxios(path, json=>{
+		const type = (navigator.userAgent.match(/iPhone|Android.+Mobile/))?"sp":"pc";
+		const places = [
+			{"ad": "banner", "id": "ad_banner"},
+			{"ad": "rectangle", "id": "ad_rectangle"}];
+		for(place of places){
+			const ad = place["ad"];
+			const id = place["id"];
+			const params = json[ad][type];
+			(window.adsbyimobile=window.adsbyimobile||[]).push({
+				pid:params["pid"], mid:params["mid"], asid:params["asid"], 
+				type:"banner", display:"inline", elementid:id});
+		}
+		const elem = document.getElementsByTagName("body")[0];
+		const imobile = document.createElement("script");
+		imobile.src = "//imp-adedge.i-mobile.co.jp/script/v1/spot.js?20220104";
+		imobile.setAttribute("async", "true");
+		elem.appendChild(imobile);
+	}, (err)=>{
+		showToast("Error", "0 min ago", "通信エラーです");
+	});
+}
+
+//==========
+// Modal
+const btnModal = document.getElementById("btn_modal");
+btnModal.addEventListener("click", ()=>{
+	showRetryDialog();
+});
+
+const btnRetry = document.getElementById("btn_retry");
+btnRetry.addEventListener("click", ()=>{
+	location.reload();// Reload
+});
+
+function showRetryDialog(){
+	const elem = document.getElementById("myModal");
+	if(elem.classList.contains("show")) return;
+	const modal = new bootstrap.Modal(elem);
+	bootstrap.Modal.getInstance(elem).show();
+}
+
+//==========
+// Button
+class Button{
+
+	constructor(x, y, w, h, str="HELLO", 
+		color="#444444", visible=true, onPressed=null){
+		this._pos       = {x: x, y: y};
+		this._w         = w;
+		this._h         = h;
+		this._str       = str;
+		this._color     = color;
+		this._visible   = visible;
+		this._onPressed = onPressed;
+	}
+
+	isInside(tX, tY){
+		if(tX < this._pos.x-this._w/2) return false;
+		if(tY < this._pos.y-this._h/2) return false;
+		if(this._pos.x+this._w/2 < tX) return false;
+		if(this._pos.y+this._h/2 < tY) return false;
+		return true;
+	}
+
+	touch(tX, tY){
+		if(!this.isInside(tX, tY)) return false;
+		if(!this._visible) return false;
+		if(this._onPressed) this._onPressed();
+		return true;
+	}
+
+	show(){
+		if(this._visible) return;
+		this._visible = true;
+	}
+
+	update(){
+		if(!this._visible) return false;
+		fill(this._color);
+		noStroke();
+		rectMode(CENTER, CENTER);
+		rect(this._pos.x, this._pos.y, this._w, this._h, this._h*0.1);
+		fill("white"); noStroke();
+		textSize(this._h*0.5); textAlign(CENTER, CENTER);
+		text(this._str, this._pos.x, this._pos.y-this._h*0.04);
+	}
+}
+
+//==========
+// Card
+class Card{
+
+	constructor(fileClose, fileOpen, x, y, size){
+		this._pageClose = new OnePage(fileClose, x, y, size);
+		this._pageOpen = new OnePage(fileOpen, x, y, size);
+		this._pageCurrent = this._pageClose;
+		this._finishFlg = false;
+		this._num = Number(fileOpen.split("_")[2].split(".")[0]);
+	}
+
+	get x(){return this._pageCurrent.x;}
+	get y(){return this._pageCurrent.y;}
+	get num(){return this._num;}
+
+	contains(x, y){return this._pageCurrent.contains(x, y);}
+
+	setPosition(x, y){
+		this._pageClose.x = x;
+		this._pageClose.y = y;
+		this._pageOpen.x = x;
+		this._pageOpen.y = y;
+	}
+
+	isOpened(){return this._pageCurrent == this._pageOpen;}
+
+	isClosed(){return this._pageCurrent == this._pageClose;}
+
+	isFinished(){return this._finishFlg;}
+
+	open(jumpH){
+		if(this.isOpened()) return;
+		this._pageCurrent = this._pageOpen;
+		this._pageCurrent.jump(jumpH, ()=>{
+			console.log("opened:", this._num);
+		});
+	}
+
+	close(shakeW){
+		if(this.isClosed()) return;
+		this._pageCurrent = this._pageClose;
+		this._pageCurrent.shake(shakeW, ()=>{
+			console.log("closed:", this._num);
+		});
+	}
+
+	finish(){
+		if(this._finishFlg) return;
+		this._finishFlg = true;
+		this._pageCurrent.alpha = 100;// Alpha
+	}
+
+	toggle(jumpH, shakeW){
+		if(this.isClosed()){
+			this.open(jumpH);
+			return;
+		}
+		if(this.isOpened()){
+			this.close(shakeW);
+			return;
+		}
+	}
+
+	update(){
+		this._pageCurrent.update();
+	}
+}
+
+class OnePage extends Sprite{
+
+	constructor(file, x, y, size, alpha=255, rotation=0){
+		super(file, x, y, size, alpha, rotation);
+		this._movingFlg = false;
+	}
+
+	isMoving(){return this._movingFlg;}
+
+	jump(jumpH=0, onFinished=null){
+		if(this._movingFlg) return;
+		this._movingFlg = true;
+		// Tween
+		const jumpY  = this._pos.y - jumpH;
+		const defX   = this._pos.x;
+		const defY   = this._pos.y;
+		const delay  = 100;
+		const tween1 = new TWEEN.Tween(this._pos)
+			.to({x: defX, y: jumpY}, delay)
+			.easing(TWEEN.Easing.Quadratic.In);
+		const tween2 = new TWEEN.Tween(this._pos)
+			.to({x: defX, y: defY}, delay)
+			.easing(TWEEN.Easing.Quadratic.Out)
+			.onComplete(()=>{
+				this._movingFlg = false;
+				if(onFinished) onFinished();// Callback
+			});
+		tween1.chain(tween2);// Chain
+		tween1.start();
+	}
+
+	shake(shakeW=0, onFinished=null){
+		if(this._movingFlg) return;
+		this._movingFlg = true;
+		// Shake
+		const shakeX = (random()<0.5)?shakeW:-shakeW;
+		const defX   = this._pos.x;
+		const defY   = this._pos.y;
+		const delay  = 50;
+		const tween1 = new TWEEN.Tween(this._pos)
+			.to({x: defX-shakeX, y: defY}, delay)
+			.easing(TWEEN.Easing.Quadratic.In);
+		const tween2 = new TWEEN.Tween(this._pos)
+			.to({x: defX, y: defY}, delay)
+			.easing(TWEEN.Easing.Quadratic.Out);
+		const tween3 = new TWEEN.Tween(this._pos)
+			.to({x: defX+shakeX, y: defY}, delay)
+			.easing(TWEEN.Easing.Quadratic.In);
+		const tween4 = new TWEEN.Tween(this._pos)
+			.to({x: defX, y: defY}, delay)
+			.easing(TWEEN.Easing.Quadratic.Out)
+			.onComplete(()=>{
+				this._movingFlg = false;
+				if(onFinished) onFinished();// Callback
+			});
+		tween1.chain(tween2);// Chain
+		tween2.chain(tween3);
+		tween3.chain(tween4);
+		tween1.start();
+	}
+}
